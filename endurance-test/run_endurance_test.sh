ade6594706c0056776746f36b1ef9a122ac3bba0
Massn <massn23579@gmail.com>
2025-08-05 15:08:36 +0900

    feat(endurance-test): execute tests at regular intervals

--- endurance-test/run_endurance_test.sh
+++ endurance-test/run_endurance_test.sh
@@ -6,9 +6,12 @@ PID_FILE="endurance_test.pid"
 # Write the PID to the PID file. This will be created in the CWD.
 echo $$ > "$PID_FILE"
 
+INTERVAL_NS=$((10 * 1000000000)) # 10 seconds in nanoseconds
+
 while true
 do
-    start_time=$(date +%s%N)
+    start_time_ns=$(date +%s%N)
+
     echo "========================================"
     echo "Test started at: $(date)"
     echo "========================================"
@@ -17,10 +20,24 @@ do
     if [ $RESULT -ne 0 ]; then
         echo "runn command failed with exit code $RESULT at $(date)"
     fi
-    end_time=$(date +%s%N)
+    end_time_ns=$(date +%s%N)
+    execution_time_ns=$((end_time_ns - start_time_ns))
+    execution_time_s=$(echo "scale=9; $execution_time_ns / 1000000000" | bc)
     echo "========================================"
     echo "Test finished at: $(date)"
-    echo "Execution time: $((($end_time - $start_time) / 1000000)) ms"
-    echo "========================================"
-    printf "\n\n"
+    echo "Execution time: ${execution_time_s} s"
+    
+    wait_time_ns=$((INTERVAL_NS - execution_time_ns))
+
+    if [ "$wait_time_ns" -gt 0 ]; then
+        wait_time_s=$(echo "scale=9; $wait_time_ns / 1000000000" | bc)
+        echo "Wait time: ${wait_time_s} s"
+        echo "========================================"
+        printf "\n\n"
+        sleep "$wait_time_s"
+    else
+        echo "Wait time: 0 s"
+        echo "========================================"
+        printf "\n\n"
+    fi
 done
